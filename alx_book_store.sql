bfccc5013465ff57f0434f7db994772c393b1e36
Consolata <consolatawanjiru45@gmail.com>
2024-10-17 13:28:46 +0300

    Create table

---
+++ alx_book_store.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE BOOKS (
+    book_id INTEGER PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INTEGER,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+
+);
+
+CREATE TABLE Authors (
+    author_id INTEGER PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id INTEGER PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INTEGER PRIMARY KEY,
+    customer_id INTEGER,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE order_details (
+    order_detail_id INTEGER PRIMARY KEY,
+    order_id INTEGER,
+    book_id INTEGER,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
